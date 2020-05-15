ee83a24de1aa38889b7828df6a8c93aef1b6a575
Felipe Vilella <33723829+felipevilella@users.noreply.github.com>
2020-05-15 18:28:56 -0300

    Felioe Vilella
    Busca de usuario por coversa, envio de gif e mensagem de erro quando o remetente envia gif, video e figurinha

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
+from random import randrange
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import time, re
-
-#grupos = 'SI - g5 🐟☠'
 
 class WhatsappBot:
     def __init__(self):
@@ -13,15 +13,13 @@ class WhatsappBot:
         time.sleep(15)
 
     def pesquisarNome(self, nome):
+        novaConversa = self.driver.find_elements_by_xpath('//div[@class="rAUz7"]')
+        novaConversa[1].click()
         participante = self.driver.find_element_by_class_name('_2S1VP')
         participante.send_keys(nome)
         time.sleep(2)
         user = self.driver.find_element_by_xpath('//span[@title="{}"]'.format(nome))
         user.click()
-
-        participante.clear()
-
-
     
     def enviarMensagem(self, mensagem):  
         mensage_caixa = self.driver.find_element_by_xpath('//div[@class="_1Plpp"]')
@@ -29,20 +27,47 @@ class WhatsappBot:
 
         enviar = self.driver.find_element_by_xpath('//button[@class="_35EW6"]')
         enviar.click()
+
+    def enviarGif(self, nomeGif) :
+        abaconteudos = self.driver.find_elements_by_xpath('//div[@class="weEq5"]')
+        abaconteudos[0].click()
+        time.sleep(1)
+
+        abaGif = self.driver.find_element_by_class_name('_1oRJg')
+        abaGif.click()
+        time.sleep(2)
+
+        campo_pesquisa = self.driver.find_element_by_class_name('_2U5s1')
+        campo_pesquisa.send_keys(nomeGif)
+        time.sleep(2)
+        
+        abaGif = self.driver.find_elements_by_xpath('//div[@class="zl5TR"]')
+        abaGif[randrange(1, 4)].click()
+        time.sleep(2)
+
+        enviar = self.driver.find_element_by_xpath('//div[@class="_3nfoJ"]')
+        enviar.click()
+        
     
     def obterUltimaMensagem(self):
         # Conteudo mensagem
+        #message-in
         mensagens = self.driver.find_elements_by_class_name("_3_7SH")
         ultimaMensagem = len(mensagens) - 1
-        mensagem = mensagens[ultimaMensagem].find_element_by_css_selector('span.selectable-text').text
 
-        # Rementente
-        Rementente_mensagem = self.driver.find_elements_by_xpath('//div[@class="copyable-text"]')
-        ultimoRemetente = len(Rementente_mensagem) -1 
-        rementente = Rementente_mensagem[ultimoRemetente].get_attribute('data-pre-plain-text')
-        nome = re.findall(r"[a-zA-Z]", rementente)
+        try:
+            mensagem = mensagens[ultimaMensagem].find_element_by_css_selector('span.selectable-text').text
 
-        return [nome, mensagem]
+            # Rementente
+            Rementente_mensagem = self.driver.find_elements_by_xpath('//div[@class="copyable-text"]')
+            ultimoRemetente = len(Rementente_mensagem) -1 
+            
+            rementente = Rementente_mensagem[ultimoRemetente].get_attribute('data-pre-plain-text')
+            nome = re.findall(r"[a-zA-Z]", rementente)
+
+            return ['success', nome, mensagem] 
+        except NoSuchElementException:
+            return ['alert', nome, 'Infelizmente, não consigo identificar o tipo de mensagem que você me enviou.']
 
     def abrirAba(self):
         self.driver.execute_script("window.open('https://accounts.google.com/signin/oauth/oauthchooseaccount?state=%7B%22csrf_token%22%3A%20%22628b041b6c99b29d0846867ba5626ce3863a25ae29c72b1c7a3c5a38ac478506%22%2C%20%22return_url%22%3A%20%22https%3A%2F%2Fdialogflow.com%2F%22%7D&redirect_uri=https%3A%2F%2Fdialogflow.com%2Foauth2callback&prompt=select_account&response_type=code&client_id=157101835696-ooapojlodmuabs2do2vuhhnf90bccmoi.apps.googleusercontent.com&scope=openid%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fgoogledevelopers%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&access_type=online&o2v=2&as=GtbyJPnOlpf5qDIzhPFf8Q&flowName=GeneralOAuthFlow', '_blank')")
@@ -85,7 +110,7 @@ if __name__ == "__main__":
     # ABRIR WhatsApp 
     WhatsappBot = WhatsappBot()
     time.sleep(10)
-    alunos = ['Samuel Mendes', 'Kamila']
+    alunos = ['Samuel']
      
     #Abrir dialogFlow
     WhatsappBot.abrirAba()
@@ -101,6 +126,8 @@ if __name__ == "__main__":
             WhatsappBot.pesquisarNome(aluno)
             time.sleep(3)
 
+            mensagem = WhatsappBot.obterUltimaMensagem()
+
             # Formatar nome aluno em array
             nomeAluno = re.findall(r"[a-zA-Z]", aluno)
             destinatario = True
@@ -108,26 +135,40 @@ if __name__ == "__main__":
             mensagem = WhatsappBot.obterUltimaMensagem()
 
             for letra in nomeAluno :
-                if(letra not in mensagem[0]) :
+                if(letra not in mensagem[1]) :
                     destinatario = False
 
             if(destinatario):
-               
-                mensagem[1] = mensagem[1].lower()
-                matches = re.findall('#debora', mensagem[1])
 
-                if matches:
-                    conversa = re.split(r'#debora', mensagem[1])
+                if mensagem[0] == 'success':
+                    mensagem[2] = mensagem[2].lower()
+                    # Verificar Quiz
+                    quiz = re.findall('#quiz', mensagem[2])
+                    # Gerar Relatorio
+                    relatorio = re.findall('#gerarRelatorio', mensagem[2])
+                    
+                    if quiz:
+                        mensage[2] = re.split(r'#quiz', mensagem[2])
 
+                    if relatorio:
+                        print('aaaa')
+                    
                     WhatsappBot.mudarAbaDialogFlow()
                     time.sleep(2)
                     
-                    WhatsappBot.digitarMensagemDialogFlow(conversa[1])
+                    WhatsappBot.digitarMensagemDialogFlow(mensagem[2])
                     time.sleep(2)
                     
                     mensagemRobo = WhatsappBot.obterRespostaDialogFlow()
                     WhatsappBot.mudarAbaWhatssap()
                     time.sleep(2)
-                    
-                    WhatsappBot.enviarMensagem("Debora: "+ mensagemRobo)
-                    time.sleep(4)
+
+                    if re.findall('enviar-gif-ok', mensagem[2]):
+                        WhatsappBot.enviarGif(mensagemRobo)
+                    else:
+                        WhatsappBot.enviarMensagem("Debora: "+ mensagemRobo)
+                
+                else:
+                    WhatsappBot.enviarMensagem("Debora: "+ mensagem[2])
+                
+                time.sleep(4)
